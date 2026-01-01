a8b5b04da440748a5e8f078e1f848bde7f710868
1llusion <evge79a@gmail.com>
2026-01-01 21:12:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Version outdated, Free & Paid Version you can find here:
-print("Find us and get the best Order Management System on Discord here:")
+# Version outdated, only ONE and ONLY NON-LEAKED Version you can find here:
+print("Find us and get the best and advanced non-leaked Slot Bot on Discord here:")
 print("https://discord.gg/RwNtpAJwfr")
 print("If the Link should be outdated, contact me via Discord!")
 print("https://guns.lol/1lsn")
